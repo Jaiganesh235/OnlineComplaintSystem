7f69513369fffcae7eac872d9e549c4514beef44
Jaiganesh235 <jaiganeshs539@gmail.com>
2025-04-29 21:05:00 +0530

    Initial commit

---
+++ src/main/java/com/example/onlinecsp/OnlinecspApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinecsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinecspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinecspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/onlinecsp/OnlinecspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinecsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinecspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
