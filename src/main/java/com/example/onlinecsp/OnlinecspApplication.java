099c20374a28af16ff942d6812e3958746c0016d
Jaiganesh235 <jaiganeshs539@gmail.com>
2025-04-30 10:15:15 +0530

    for jai

--- src/main/java/com/example/onlinecsp/OnlinecspApplication.java
+++ src/main/java/com/example/onlinecsp/OnlinecspApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OnlineCspApplication {
+public class OnlinecspApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OnlineCspApplication.class, args);
+        SpringApplication.run(OnlinecspApplication.class, args);
     }
 }
